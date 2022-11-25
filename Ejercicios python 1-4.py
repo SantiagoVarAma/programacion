ef63fbbb2d8e03ee6097f986932445e22d4e071f
SantiagoVarAma <116911534+SantiagoVarAma@users.noreply.github.com>
2022-11-24 21:51:25 -0600

    Add files via upload

---
+++ Ejercicios python 1-4.py
@@ -0,0 +1,69 @@
+#print ("Hola Mundo")
+#n= (1+2)
+#x= 15.3
+#res= (n*x)
+#print ("El resultado de la multiplicacion es igual a", res)
+
+
+#A= (8+2)
+#B= (10-8)
+#C= (2+4)
+#Definir la variable Resultado
+#Resultado= (A*B+C)
+#Se imprime la variable resultado 
+#Print ("El resultado de la variable es")
+
+print ("Ingresar su nombre sin espacios")
+nombre = str
+nombre= input (nombre)
+print ("Su nombre sin espacios es : , Nombre")
+
+
+
+
+#ejercicio_2
+a=5
+b=3
+c=4
+d=8
+
+print("Resultado de la operacion es ")
+Resultado = ((a+b-c)/(b*c)%(c+b))
+print("EL resultado final" , Resultado)
+
+
+#ejercicio basado en datos compuestos 
+
+
+
+Arreglo1 =((4,7),(4,5),(2,9),(5,8))
+
+Arreglo2 = ((5,7),(7,9),(0,5),(1,7), Arreglo1)
+
+SumatoriaA = (Arreglo1,Arreglo2) 
+
+print("La sumatoria de arreglos es", SumatoriaA)
+
+Arreglo3 =((5,8),(8,51),(7,7),(8,8))
+ 
+Arreglo4 = ((6,9),(7,9),(6,4),(8,9))
+
+SumatoriaB = (Arreglo3,Arreglo4) 
+print("La sumatoria de arreglos es", SumatoriaB)
+
+
+#Ejercicio_4
+x = input()
+if(type (x) is int):
+    print("es entero")
+    if(x<18):
+        print("es menor de edad")
+    elif(x==18):
+        print("Apenas es adulto")
+    else:
+        print("es mayor de edad")
+else:
+    print("no es entero")
+
+
+
